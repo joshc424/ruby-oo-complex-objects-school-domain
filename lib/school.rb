3b9c7d81449d7c6756cdfb32422c0e60997007de
Joshua Cho <joshdavecho@protonmail.com>
2021-01-25 11:17:59 -0500

    commit tree bdceb549a7f5d2da75f7f16373ce8fb4bada7cd4

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+require 'pry'
+class School
+
+    attr_reader :name, :roster
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(student_names, grade)
+        if @roster[grade].has_key?(grade)
+            @roster[grade] << student_names
+        end
+    end
+
+end
